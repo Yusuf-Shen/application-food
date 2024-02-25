05baa6d2c47df2f679f9fdbbdfe07edffb2676ac
Yusuf ma <13893491334mys@gmail.com>
2024-02-25 17:02:16 +1000

    Set up environment and pages folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from '../pages/Home'
 
 export default function App() {
-  return (
-    <div>App</div>
-  )
+  return <BrowserRouter>
+  <Routes>
+    <Route path='' element={<Home/>}></Route>
+    <Route path='' element={<Home/>}></Route>
+    <Route path='' element={<Home/>}></Route>
+    <Route path='' element={<Home/>}></Route>
+  </Routes>
+  
+  
+  </BrowserRouter>
 }
 
